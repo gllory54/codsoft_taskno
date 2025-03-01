ddad6e7d5d7c812d980493a8724f6d59225f3043
gllory54 <gllorybolanle@gmail.com>
2025-03-01 20:02:48 +0100

    Add files via upload

---
+++ CODSOFT/codsoft_task 3/Calculator/script.js
@@ -0,0 +1,24 @@
+function clearDisplay(){
+    document.getElementById('display').innerText = '0';
+}
+
+function appendToDisplay(value){
+    const display = document.getElementById('display');
+    if(display.innerText === '0'){
+        display.innerText = value;
+    }
+    else{
+        display.innerText += value;
+    }
+}
+
+function calculateResult(){
+    const Display = document.getElementById('display');
+    try{
+        display.innerText =
+        eval(display.innerText);
+    }
+    catch(e){
+        display.innerText = 'Error'
+    }
+}
